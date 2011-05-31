75f8989d2b3b5b16b59033cf9fdd196786a43f69
Soundarya Chandar <soundarya.c@gmail.com>
2011-05-31 18:48:32 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodCamp::Application.config.secret_token = '1977ab538db55e4e2593c70be585f75f0d362db518432a69187911236043d45bdd75d1ca208ae7c4a2490303a1930bcefa665d7cba48aa459ee85286021380bf'
