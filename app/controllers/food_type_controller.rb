c12542081a9a96e066b873522d0b53c348af31f9
Soundarya Chandar <soundarya.c@gmail.com>
2011-10-06 00:23:59 -0400

    Updates

--- app/controllers/food_type_controller.rb
+++ app/controllers/food_type_controller.rb
@@ -6,12 +6,31 @@ class FoodTypeController < ApplicationController
   end
 
   def upload_file
-    @file_object = params[:upload][:food_type]
-    @csv_contents = CSV.read(@file_object)
-     @csv_contents.each do |row|
-      FoodType.create!(:foodkind => row[0],:name => row[1],:location => row[2],:city => row[3], :tag_list => row[0])
-    end
-    redirect_to food_type_index_path
+   if params[:upload].present? then 
+     @file_object = params[:upload][:food_type]
+
+     if valid_csv_file?(@file_object) then
+    
+       @csv_contents = CSV.read(@file_object)
+       @headers = @csv_contents.shift
+       @csv_contents.each do |row|
+          FoodType.create!(
+                           :foodkind => row[0],
+                           :name => row[1],
+                           :location => row[2],
+                           :city => row[3],
+                           :tag_list => row[0])
+        end
+       flash[:success] = "File Upload was successful"
+       redirect_to food_type_index_path  
+     else
+       flash[:error] = "Invalid CSV file. Please check the file."
+       render 'upload'
+     end
+   else
+     flash[:error] = "Invalid File. Please select a file to upload"
+     render 'upload'
+   end 
   end
 
   def index
@@ -24,5 +43,14 @@ class FoodTypeController < ApplicationController
 
   def home
     @title = "Home"
+  end
+  
+
+  def valid_csv_file?(file)   
+    if File.extname(file) == ".csv" then
+      return true
+    else
+      return false      
+    end
   end
 end
--- spec/controllers/food_type_controller_spec.rb
+++ spec/controllers/food_type_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'csv'
 
 describe FoodTypeController do
 
@@ -11,11 +12,79 @@ describe FoodTypeController do
       get 'upload'
       response.should be_success
     end
+  end
 
-    it "should have the title 'File Upload'" do |object|
-      get :upload
-      response.should have_selector("title",:content => "File Upload")
+  describe "POST 'upload'" do
+
+    file_path = "/Users/soundaryachandar/rails_projects/Food-Camp/spec/helpers/"
+   
+    describe "success" do
+      before(:each) do
+         @attr = { :food_type => Rack::Test::UploadedFile.new(file_path + "test.csv","multipart/form-data") }
+      end
+      
+      
+      it "should render the index template" do
+        post :upload_file,:upload => @attr
+        response.should render_template('index')
+      end
+
+      it "should upload the contents and change the count" do
+        lambda do
+          post :upload_file,:upload => @attr
+        end.should change(FoodType,:count)
+      end
+
+      it "should have a flash message stating file upload success" do
+        post :upload_file, :upload => @attr
+        flash[:success].should =~ /successful/i
+      end
+      
     end
+    describe "failure" do
+      
+      describe "csv validation for invalid file extensions" do
+        
+        before(:each) do
+          @attr = { :food_type => Rack::Test::UploadedFile.new(file_path + "test.dat","multipart/form-data") }
+        end
+      
+        it "should reject a non-csv file" do
+          post :upload_file, :upload => @attr
+          response.should render_template('upload')
+        end
+
+        it "should flash an error message" do
+          post :upload_file, :upload => @attr
+          flash[:error].should =~ /invalid/i
+        end
+
+        it "should not upload the file contents" do
+          lambda do
+            post :upload_file,:upload => @attr
+          end.should_not change(FoodType,:count)
+        end
+      end #invalid file extensions
+      
+      describe "csv validation for no file selected" do
+        
+        it "should upload only when a file is selected" do
+          post :upload_file
+          response.should render_template('upload')
+        end
+
+        it "should display a flash error message when no file is selected" do
+          post :upload_file
+          flash[:error].should =~ /invalid/i
+        end
+
+        it "should not change the count of FoodType" do
+          lambda do
+            post :upload_file
+         end.should_not change(FoodType,:count) 
+        end
+      end #no file selected
+    end #failure
   end
 
   describe "GET 'home'" do
